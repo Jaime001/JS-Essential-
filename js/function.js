4abfe10ff473f631b4570a6b8565915137ea26db
Jaime001 <jortiz10@ucol.mx>
2020-12-13 19:23:06 -0600

    Basic functions examples

---
+++ js/function.js
@@ -0,0 +1,25 @@
+function getTheBiggestValue(a, b){
+  a>b ? console.log("The biggest value is: " + a) : console.log("the biggest value is: " + b);
+}
+getTheBiggestValue(10, 20);
+
+function sum(a,b){
+  return a + b 
+}
+
+function rest(a,b){
+  return a - b 
+}
+
+function div(a,b){
+  return a / b 
+}
+
+function mult(a,b){
+  return a * b 
+}
+
+console.log(sum(30, 2));
+console.log(rest(30, 2));
+console.log(div(30, 2));
+console.log(mult(30, 2));
