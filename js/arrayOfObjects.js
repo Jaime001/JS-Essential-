0c3a00feff2312d8e310147798e9de1c0cfca1a5
Jaime001 <jortiz10@ucol.mx>
2020-12-15 15:40:06 -0600

    loop an array of objects in js

---
+++ js/arrayOfObjects.js
@@ -0,0 +1,36 @@
+var ximena = {
+  name: "Ximena",
+  age: 20,
+  sex: "female"
+}
+
+var jaime = {
+  name: "Jaime",
+  age: 22,
+  sex: "male"
+}
+
+var oswaldo = {
+  name: "Oswaldo",
+  age: 15,
+  sex: "male"
+}
+
+var jorge = {
+  name: "Jorge",
+  age: 23,
+  sex: "male"
+}
+
+var paty = {
+  name: "Paty",
+  age: 53,
+  sex: "female"
+}
+
+
+var people = [ximena, jaime, oswaldo, jorge, paty]
+
+for (var i = 0; i < people.length; i++){
+  console.log(`${people[i].name} has ${people[i].age} and is a ${people[i].sex}`);
+}
