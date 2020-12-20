7e1f44a31e34490e368b09620ce1ef859421f35b
Jaime001 <jortiz10@ucol.mx>
2020-12-20 14:34:31 -0600

    Promises in JS for solve callbacks Hell

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,30 +1,28 @@
-  const API_URL = 'https://swapi.dev/api/'
-  const PEOPLE_ID = 'people/:id'
-  const opts = { crossDomain: true }
+const API_URL = 'https://swapi.dev/api/'
+const PEOPLE_ID = 'people/:id'
+const opts = { crossDomain: true } //upload the info or date of another site.
 
-  //This get method call the api and the id of the object that we want to get
-  //$.get(`${API_URL}${PEOPLE_ID.replace(":id", i)}`, { crossDomain: true})-
-  // const lukeUrl = `${API_URL}${PEOPLE_ID.replace(':id', 1)}`
-  //arguments is a variable that all the functions have and return an array with all the parameters that receive that function
-  function getCharacter (id, callback) {
-      const displayCharacter = `${API_URL}${PEOPLE_ID.replace(':id', id)}`
-
-      $.get(displayCharacter, opts, callback)
-      .fail(() => { console.log(`Error ocurred with the character ${id}`);})
-  }
-
-  getCharacter(1, function (character){
-    console.log(`Hello, I'm ${character.name}`)
-      getCharacter(2, function(character){
-        console.log(`Hello, I'm ${character.name}`)
-        getCharacter(3, function(character){
-          console.log(`Hello, I'm ${character.name}`)
-          getCharacter(4, function(character){
-            console.log(`Hello, I'm ${character.name}`)
-            getCharacter(5, function(character){
-              console.log(`Hello, I'm ${character.name}`)
-            })
-          })
-        })
-      })
+//This get method call the api and the id of the object that we want to get
+//$.get(`${API_URL}${PEOPLE_ID.replace(":id", i)}`, { crossDomain: true})-
+// const lukeUrl = `${API_URL}${PEOPLE_ID.replace(':id', 1)}`
+//arguments is a variable that all the functions have and return an array with all the parameters that receive that function
+//callback is a fucntion wich get the array of caracteristics from the character 
+function getCharacter (id) {
+  return new Promise((resolve, reject) => {
+    const displayCharacter = `${API_URL}${PEOPLE_ID.replace(':id', id)}`
+    $.get(displayCharacter, opts, function(data){
+      resolve(data)
+    })
+    .fail(() =>  reject(id))
   })
+}
+function onError(id) {
+  console.log(`We have an error with the character ${id}`);
+}
+
+function onResolve(character){
+  console.log(`The character 1 is: ${character.name}`);
+}
+getCharacter(1)
+  .then(onResolve)
+  .catch(onError)
