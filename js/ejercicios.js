8f31f8ff733ed3f597314b3c44921601ccf03dcc
Jaime001 <jortiz10@ucol.mx>
2020-12-22 17:20:35 -0600

    querySelector and nth:child(odd-even) in JS

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -5,13 +5,14 @@ const description = document.getElementById('description')
 description.innerHTML = "Listado de cosas "
 
 //Como obtener elementos por Clase
-
 //const items = document.getElementsByClassName('list-group-item')//El getElementsByClassName devuelve un listado(array) de los objetos con esa clase
-const items = document.getElementsByTagName('li')
 
+//odd - par y even - impar querySelectorAll retorna un arreglo y sin el All solo el primer elemnto. :nth-child() es para acceder a la posicion o hijos del arreglo
+const items = document.querySelectorAll('li:nth-child(odd)')
 for (let i = 0; i < items.length; i++) {
-  if (i % 2 == 0) {
-    let elements = items[i];
-    elements.style.background = '#f2f2f2'
-  }
+  let elements = items[i];
+  elements.style.background = '#f2f2f2'
 }
+
+const element = document.querySelector('div.row > ul.list-group > li')
+console.log(element);
