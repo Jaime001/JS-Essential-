0719a60367ecbe8930206d90ca53e1a97287c3ee
Jaime001 <jortiz10@ucol.mx>
2020-12-23 10:38:11 -0600

    Nodes in JS and how they work

---
+++ js/nodes.js
@@ -0,0 +1,4 @@
+const element = document.getElementById('first-course')
+console.log(element.childElementCount)
+console.log(element.childNodes.length) //El nodo es el texto del li(Python)
+console.log(element.childNodes);
