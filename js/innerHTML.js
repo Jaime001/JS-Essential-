086dc20c1746351abe499c63376bccdcb0bd876e
Jaime001 <jortiz10@ucol.mx>
2020-12-23 10:59:24 -0600

    innerHMTL and textContent in JS

---
+++ js/innerHTML.js
@@ -0,0 +1,4 @@
+
+const description = document.getElementById('description')
+description.textContent = 'Nueva descripcion'
+description.innerHTML = '<strong>' + description.textContent + '</strong>'
